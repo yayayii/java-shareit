8d9dbcf3779aab41b4dd3ff700c49365b9ebf170
yayayi <evgegnii-homichk@yandex.ru>
2023-02-27 18:47:59 +0300

    feat: added fields for models

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private int id;
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+    private String request;
 }
