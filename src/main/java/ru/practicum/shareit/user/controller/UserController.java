84e18ecfc8db418d3c23a21a38f6a2ba3aef2953
yayayi <evgegnii-homichk@yandex.ru>
2023-02-27 23:13:19 +0300

    feat: added mapping for users

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,12 +1,46 @@
 package ru.practicum.shareit.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.Collection;
 
 /**
  * TODO Sprint add-controllers.
  */
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService userService;
+    //create
+    @PostMapping
+    User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+    //read
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable int userId) {
+        return userService.getUser(userId);
+    }
+    @GetMapping
+    Collection<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    //update
+    @PatchMapping
+    User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+    //delete
+    @DeleteMapping("/{userId}")
+    void deleteUser(@PathVariable int userId) {
+        userService.deleteUser(userId);
+    }
+    @DeleteMapping
+    void deleteAllUsers() {
+        userService.deleteAllUsers();
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.user.service;
+
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Collection;
+
+public interface UserService {
+    //create
+    User addUser(User user);
+    //read
+    User getUser(int userId);
+    Collection<User> getAllUsers();
+    //update
+    User updateUser(User user);
+    //delete
+    void deleteUser(int userId);
+    void deleteAllUsers();
+}
